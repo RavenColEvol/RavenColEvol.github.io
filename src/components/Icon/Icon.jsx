8e931b7f960475f5556192303efccb08577807f2
RavenColEvol <ravilamkoti@gmail.com>
2024-01-07 14:32:02 +0530

    move to astro

--- src/components/Icon/Icon.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import IcoMoon from "react-icomoon";
-import iconSet from "../../static/Icons/selection.json";
-
-const Icon = (props) => <IcoMoon iconSet={iconSet} {...props} />;
-
-export default Icon;
--- src/utils/themeContext.js
+++
@@ -1,6 +0,0 @@
-import { useContext, createContext } from "react";
-
-
-const ThemeContext = createContext('dark');
-export const useTheme = () => useContext(ThemeContext);
-export const ThemeSwitchProvider = ThemeContext.Provider;
