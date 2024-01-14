5f71af33c3f82787f71b0cbd6d63e31ec5dc2972
RavenColEvol <ravilamkoti@gmail.com>
2024-01-14 23:08:08 +0530

    add: g-analytics with partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown'
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ravencolevol.github.io',
-  integrations: [react()]
+  integrations: [
+    react(),
+    partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		}),
+  ]
 });
